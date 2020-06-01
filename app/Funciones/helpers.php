a3fba48fa4e0d59fe813a7cc6b109bf30f68c213
FranciscoJoseVerduB <1905335@alu.murciaeduca.es>
2020-06-02 00:38:05 +0200

    Reubicacion del proyecto en github

--- app/Funciones/helpers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function setActive($ruta){
-    return request()->routeIs($ruta) ? 'active': '';
-}
-
-function setSelectedCombobox($idEntidadInicial = 0, $idEntidadAComparar = 0){
-    return $idEntidadInicial === $idEntidadAComparar? 'selected': '';
-}
--- resources/views/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-        <div class="py-4">
-        </div>
-        <footer class="footer fixed-bottom bg-white text-center py-2 shadow">
-            {{ config('app.name') }} | Copyright @ {{ date('Y') }}
-            @auth 
-                 -- Usuario identificado: <strong>{{Auth::user()->nombre }}</strong>
-            @endauth 
-        </footer>
